75592a3f2e62f8e04fd8472acac6bf68226ae5a5
zcmk123 <214778293@qq.com>
2018-01-17 17:55:15 +0800

    20180117仿豆瓣电影

---
+++ douban/js/index.js
@@ -0,0 +1,44 @@
+(function () {
+    var searchSug = $('.search-suggest'),
+        timer = null;
+
+    function deBounce(func, delay) {
+        clearTimeout(timer);
+        timer = setTimeout(func, delay);
+    }
+
+    $('#input-text').keyup(function () {
+        deBounce(getDataList, 1000);
+    });
+
+    function getDataList() {
+        $.ajax({
+            url: 'https://api.douban.com/v2/movie/search?q=' + $('#input-text').val() + '&count=7',
+            dataType: 'jsonp',
+            success: renderList,
+            error: function () {
+                console.error('请求失败');
+            }
+        })
+        function renderList(data) {
+            console.log(data);
+            if (data.count > 0) {
+                searchSug.html('');
+                var tempStr = '';
+                var sgUl = $("<ul></ul>");
+                data = data.subjects;
+                data.forEach(function (ele, index) {
+                    var $Li = $("<li></li>"),
+                        $A = $("<a href='" + ele.alt + "'></a>"),
+                        $Img = $("<img src=" + ele.images.small + ">"),
+                        $P1 = $("<p>" + ele.title + "</p>");
+                    sgUl.append($Li.append($A.append($Img, $P1)));
+                })
+                sgUl.appendTo(searchSug);
+                searchSug.css('display', 'block');
+            }else {
+                searchSug.css('display', 'none');
+            }
+        }
+    }
+})()
