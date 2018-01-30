9832246a68596981d76d7936de51555c0405390f
zcmk123 <214778293@qq.com>
2018-01-30 14:08:50 +0800

    ...

--- css3Photo/js/index.js
+++ css3Photo/js/index.js
@@ -1,14 +1,16 @@
 
-setTimeout(function () {
-    $('.wrapper').removeClass('init');
-}, 100)
-$('.slice').on('click', function (e) {
-    e.stopPropagation();
-    $('.wrapper').addClass('click');
-    $(this).addClass('active');
-})
-$('.close').on('click', function (e) {
-    e.stopPropagation();
-    $('.wrapper').removeClass('click');
-    $('.slice').removeClass('active');
+window.onload(function () {
+    setTimeout(function () {
+        $('.wrapper').removeClass('init');
+    }, 100)
+    $('.slice').on('click', function (e) {
+        e.stopPropagation();
+        $('.wrapper').addClass('click');
+        $(this).addClass('active');
+    })
+    $('.close').on('click', function (e) {
+        e.stopPropagation();
+        $('.wrapper').removeClass('click');
+        $('.slice').removeClass('active');
+    })
 })
